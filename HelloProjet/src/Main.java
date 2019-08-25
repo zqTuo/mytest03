956da606ec300825dec38ea91a7ea7352f932a14
admin <970153724@qq.com>
2019-08-25 20:52:51 +0800

    第二次

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -1,9 +1,15 @@
-import org.junit.Test;
+import org.testng.annotations.Test;
+
 
 public class Main {
 
     @Test
     public void testHelloWrold() {
+
+        String a="awdaw";
         System.out.println("HelloWorld!");
+
+
+        String abc;
     }
 }
